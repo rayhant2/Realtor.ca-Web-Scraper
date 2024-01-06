18427094b15c15b1924bf96b6a6f6e185d3ff31e
Tahseen Rayhan <61428939+rayhant2@users.noreply.github.com>
2024-01-06 17:43:23 -0500

    Clean up main.py

--- Realtor Web Scraper/main.py
+++ Realtor Web Scraper/main.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support import expected_conditions as EXPECTED
 options = webdriver.ChromeOptions()
 options.add_argument('--disable-popup-blocking')
 chrome = uc.Chrome(options=options)
-wait = WebDriverWait(chrome, 30)           # Content loaded dynamically using JavaScript - waiting for the elements to be present
+wait = WebDriverWait(chrome, 30)           # Content on website loaded dynamically using JavaScript - waits for the elements to be present
 
 
 def scrape_properties():
@@ -28,22 +28,22 @@ def scrape_properties():
             property_data["bathrooms"] = icon_elements[1].text
         elif len(icon_elements) == 1:
             property_data["bedrooms"] = 'PARKING'
-            property_data["bathrooms"] = icon_elements[0].text      # If the property has no bathrooms or bedrooms, its given a placeholder of 0 bathrooms on the website
+            property_data["bathrooms"] = 'PARKING'      # Realtor.ca lists parking lots for sale as well and only list the number of bathrooms which is 0 - assigning the bed/bath data as Parking
 
         if properties.find_elements(By.XPATH, ".//div[@class='listingCardAddress']"):
             property_data["address"] = properties.find_element(By.XPATH, ".//div[@class='listingCardAddress']").text
 
-        all_properties.append(property_data)
+        all_properties.append(property_data)        # Adding the property data dictionaries into a single list
 
 
 if __name__ == "__main__":
     all_properties = []
     chrome.get("https://www.realtor.ca/map#ZoomLevel=11&Center=43.708087%2C-79.376385&LatitudeMax=43.91174&LongitudeMax=-78.85076&LatitudeMin=43.50374&LongitudeMin=-79.90201&view=list&Sort=6-D&GeoIds=g30_dpz89rm7&GeoName=Toronto%2C%20ON&PropertyTypeGroupID=1&TransactionTypeId=2&PropertySearchTypeId=0&Currency=CAD")
     scrape_properties()
-    dataFrame = pd.DataFrame.from_dict(all_properties)
+    dataFrame = pd.DataFrame.from_dict(all_properties)       # First page
     
 
-    for i in range(2, 51):
+    for i in range(2, 51):                            # Second -> 50th Page
         handles_before = chrome.window_handles
         chrome.execute_script(f"window.open('https://www.realtor.ca/map#ZoomLevel=11&Center=43.708087%2C-79.376385&LatitudeMax=43.88082&LongitudeMax=-79.05332&LatitudeMin=43.53486&LongitudeMin=-79.69945&view=list&CurrentPage={str(i)}&Sort=6-D&GeoIds=g30_dpz89rm7&GeoName=Toronto%2C%20ON&PropertyTypeGroupID=1&TransactionTypeId=2&PropertySearchTypeId=0&Currency=CAD','_blank')")
         handles_after = chrome.window_handles
@@ -56,9 +56,6 @@ if __name__ == "__main__":
         time.sleep(0.25)
         
     
-    
     print(dataFrame)
     dataFrame.to_excel('properties.xlsx')
 
-
-# Installed openpyxl, selenium-wire, selenium, undetected-chromedriver
